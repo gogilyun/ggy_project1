d0d43e769110436bd90bd7b25753d1dd8e605e95
YunJiHyun <wlgus7005@naver.com>
2015-11-03 18:24:15 +0900

    minor change1

--- StudentProgram.c
+++ StudentProgram.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-
+//1
 struct list
 {
 	char id[20];
@@ -9,6 +9,7 @@ struct list
 	char department[30];
 	char PhoneNumber[20];
 };
+
 
 int main()
 {
